5a68b3de1971ed245bb51173a36329e2e9049856
mahmoud malek <information.mahmoud@gmail.com>
2024-09-15 20:04:30 +0300

    change folder name for convension

--- get_movies_db.py
+++ get_movies_db.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-""" a module that gets the movies from API based on
+""" a module that gets the movies from API based on 
 topic provided by the user """
 
 import requests
@@ -9,25 +9,26 @@ import json
 from typing import List, Dict
 
 
+
 def get_movie(name: str) -> json:
-    """Get the movie based on the name."""
-    url = f"https://api.themoviedb.org/3/search/movie?query={name}"
+	"""Get the movie based on the name."""
+	url = f"https://api.themoviedb.org/3/search/movie?query={name}"
 
-    headers = {
-        "accept": "application/json",
-        "Authorization": os.getenv("MOVIES_API_KEY")
-    }
+	headers = {
+	 "accept": "application/json",
+	 "Authorization": os.getenv("MOVIES_API_KEY")
+	}
 
-    response = requests.get(url, headers=headers).json()
+	response = requests.get(url, headers=headers).json()
 
-    return response
+	return response
 
 
 def get_movies_list(user_request: str) -> Dict:
-    """Get the list of movies based on the user request."""
-    movies = {}
-    response = get_movie(user_request)
-    if response.get('results'):
-        for movie in response['results']:
-            movies[movie['title']] = movie
-    return movies
+	"""Get the list of movies based on the user request."""
+	movies = {}
+	response = get_movie(user_request)
+	if response.get('results'):
+		for movie in response['results']:
+			movies[movie['title']] = movie
+	return movies
