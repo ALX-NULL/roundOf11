b66ecc3c447b23f391b58ce6b29ba89c16d0a004
Yahia <yn7599@gmail.com>
2024-09-16 23:48:49 +0300

    add Not found page

--- server/web/app.py
+++ server/web/app.py
@@ -9,7 +9,15 @@ from get_movies_db import get_movies_list as movies_from_db
 from generate_quiz import generate_quiz_ai
 app = FastAPI()
 
-app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_methods=['*'], allow_headers=['*'])
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Redis Configuration
 redis_client = redis.Redis(host='localhost', port=6379)
