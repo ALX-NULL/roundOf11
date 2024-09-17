52b3773fdf524b11ee4c4d4bb861f2a46b48e538
Yahia <yn7599@gmail.com>
2024-09-17 20:09:30 +0300

    create quiz checker functionality

--- client/src/Task.tsx
+++ client/src/Task.tsx
@@ -10,7 +10,7 @@ export default function Task(task: TaskT) {
   return (
     <article className="rounded-lg px-8 border dark:border-gray-700 bg-white dark:bg-gray-900">
     <h4 className="">{task.question}</h4>
-    <ul className="flex flex-col gap-2 w-[max]">
+    <ul className="flex flex-col gap-2 w-[max] px-6">
     {Object.entries(task.choices).map(([key, val]) =>
         <label key={key} className="leading-5 py-2 has-[:checked]:font-bold has-[:checked]:border-cyan-500 dark:border-gray-700 rounded-full w-full min-w-32 px-4 cursor-pointer border bg-gray-50 dark:bg-slate-800">
         <input className="hidden" type="radio" name={task.id} value={key} />
--- client/src/TaskList.tsx
+++ client/src/TaskList.tsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { FormEventHandler, useMemo, useState } from "react";
 import SpinnerButton from "./SpinnerButton";
 import Task, { TaskT } from "./Task";
 import { getTasks } from "./utils/loaders";
 
 export default function TaskList() {
   const [tasks, setTasks] = useState<{[k: string]: TaskT}>({});
+  const totalTasks = useMemo(()=> Object.keys(tasks).length, [tasks])
   const [spining, setSpining] = useState(false);
+  const [correctAnswers, setAnswers] = useState(0);
   const updateTasks = () => {
     setSpining(true);
     getTasks().then((data) => {
@@ -14,19 +16,40 @@ export default function TaskList() {
     })
   };
 
+  const checkAnswers: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    setSpining(true);
+    const data = new FormData(e.currentTarget);
+    let correct = 0;
+    for (const [key, value] of data) {
+      await new Promise((res) => setTimeout(res, 500));
+      if (value == tasks[key]?.answer) setAnswers(++correct);
+    }
+    setSpining(false);
+  };
+
   return (
     <div className="flex flex-col my-8">
-    {Object.keys(tasks).length ?
-      <form>
+    { totalTasks ?
+      <form onSubmit={checkAnswers} >
       <hr />
       <h2>Quizzes</h2>
       <div className="flex flex-col gap-4">
       {Object.entries(tasks).map(([key, task]) => <Task key={key} {...task} id={key} />)}
       </div>
       <div className="text-center">
+      { correctAnswers > 0 &&
+      <h3>{` ${correctAnswers} / ${totalTasks}`}</h3>
+      }
+      { !spining && correctAnswers > 0 && (correctAnswers < totalTasks ?
+      <p className="-mb-7">You can do better, try again.</p>
+      : <p><strong>Well Done,</strong> You aced it.</p>)
+      }
+      { correctAnswers < totalTasks &&
       <SpinnerButton spin={spining} type="submit" spinLabel="Looking into them..." >
       Check your answers
       </SpinnerButton>
+      }
       </div>
       </form>
     :
